beda636cf32978b26c5dabb3910e62502185e67f
gurzantsingherereee <243097774+gurzantsingherereee@users.noreply.github.com>
2026-08-02 01:10:16 +0530

    Publish commerce creative studio website

---
+++ studio/config.js
@@ -0,0 +1,14 @@
+window.STUDIO_CONFIG = {
+  name: "STUDIO // 00",
+  publicEmail: "",
+  email: "",
+  formEndpoint: "",
+  integrations: {
+    crm: { provider: "", endpoint: "" },
+    notion: { databaseId: "" },
+    slack: { channelId: "" },
+    analytics: { provider: "", siteId: "" }
+  },
+  location: "New Delhi, India",
+  currency: "INR"
+};
